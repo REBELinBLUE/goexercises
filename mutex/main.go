8783df33db5798c62cb03d638f735640f1f00631
Stephen Ball <stephen.ball@sainsburys.co.uk>
2017-01-10 15:14:44 +0000

    Mutex exercise

---
+++ mutex/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var Animals = map[string]int{
+	"Dog":      0,
+	"Cat":      0,
+	"Rabbit":   0,
+	"Goldfish": 0,
+}
+
+type Cache struct {
+	sync.Mutex
+	Animals map[string]int
+}
+
+func New() *Cache {
+	return &Cache{
+		Animals: make(map[string]int),
+	}
+}
+
+func (c *Cache) Get(key string) int {
+	c.Lock()
+	defer c.Unlock()
+	return c.Animals[key]
+}
+
+func (c *Cache) Set(key string, value int) {
+	c.Lock()
+	defer c.Unlock()
+	c.Animals[key] = value
+}
+
+func main() {
+	ca := New()
+	for animal, count := range Animals {
+		ca.Set(animal, count)
+
+		for c := 0; c < 10; c++ {
+			go func(cache *Cache, animal string) {
+				s := rand.NewSource(time.Now().UnixNano())
+				r := rand.New(s)
+				num := r.Intn(1000)
+
+				cache.Set(animal, num)
+
+				//cache.Animals[animal] = num
+
+				fmt.Printf("%v set to %d\n", animal, num)
+			}(ca, animal)
+		}
+	}
+
+	time.Sleep(time.Second * 2)
+	fmt.Printf("%v", ca)
+}
